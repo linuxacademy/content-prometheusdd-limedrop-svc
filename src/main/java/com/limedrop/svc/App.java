819a2f668e6f59d13f2841e525451845af32aaa8
Will Boyd <willb@linuxacademy.com>
2020-05-01 14:11:03 -0400

    add end state

--- src/main/java/com/limedrop/svc/App.java
+++ src/main/java/com/limedrop/svc/App.java
@@ -3,11 +3,20 @@ package com.limedrop.svc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.exporter.HTTPServer;
+import java.io.IOException;
+
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+                
+                try {
+                    HTTPServer server = new HTTPServer(8081);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 	}
 
 }
--- src/main/java/com/limedrop/svc/LimeDropController.java
+++ src/main/java/com/limedrop/svc/LimeDropController.java
@@ -4,12 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+
 @RestController
 public class LimeDropController {
 
+    static final Counter totalRequests = Counter.build()
+      .name("total_requests").help("Total requests.").register();
+    static final Gauge inprogressRequests = Gauge.build()
+      .name("inprogress_requests").help("Inprogress requests.").register();
+
     @GetMapping(path="/limesAvailable", produces = "application/json")
     public String checkAvailability() {
+        inprogressRequests.inc();
+        totalRequests.inc();
+
         String response = "{\"warehouse_1\": \"58534\", \"warehouse_2\": \"72399\"}";
+
+        inprogressRequests.dec();
         return response;
     }
 
